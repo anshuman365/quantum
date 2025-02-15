708940292cf077db3f6163cd39731fa181ab958f
Anshuman Singh <jaspreetsinghaniya64@gmail.com>
2025-02-15 10:06:34 +0530

    Initial commit for quantum project

---
+++ quantum_project.py
@@ -0,0 +1,18 @@
+from qiskit import QuantumCircuit, Aer, transpile, assemble, execute
+
+# Create a quantum circuit with 1 qubit and 1 classical bit
+qc = QuantumCircuit(1, 1)
+
+# Apply Hadamard gate to create superposition
+qc.h(0)
+
+# Measure the qubit
+qc.measure(0, 0)
+
+# Run the circuit on a simulator
+simulator = Aer.get_backend('qasm_simulator')
+job = execute(qc, simulator, shots=1000)
+result = job.result()
+
+# Print results
+print(result.get_counts(qc))
